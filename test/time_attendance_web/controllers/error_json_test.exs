cba50744fd8bc4cd9393fe10c14c661bab8cd6db
Samuel C. Iheadindu <Samuel.Iheadindu@abujaelectricity.com>
2025-07-17 21:42:42 +0100

    Add function to create and fetch api token

--- test/time_attendance_web/controllers/error_json_test.exs
+++ test/time_attendance_web/controllers/error_json_test.exs
@@ -2,7 +2,9 @@ defmodule TimeAttendanceWeb.ErrorJSONTest do
   use TimeAttendanceWeb.ConnCase, async: true
 
   test "renders 404" do
-    assert TimeAttendanceWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+    assert TimeAttendanceWeb.ErrorJSON.render("404.json", %{}) == %{
+             errors: %{detail: "Not Found"}
+           }
   end
 
   test "renders 500" do
